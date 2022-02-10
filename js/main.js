54f278364a87abe2920c053d1cf5e91b72ae532c
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-10 17:18:52 -0500

    Se crea una funcion autoejecutable con variables del juego

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// inicio de la funcion auto ejecutable
+
+const game = function() {
+
+    // Se declaran las variables del juego
+
+    let time = 30; // cambiar la velocidad de la pelota ⚾
+    let movement = 20;
+    let movementBar = 20;
+    let width = document.documentElement.clientWidth - movement; // medidas del ancho de la pantalla
+    let height = document.documentElement.clientHeight - movement; // medidas de la longitud de la pantalla
+    let controlGame;
+    let player1;
+    let player2;
+}();
